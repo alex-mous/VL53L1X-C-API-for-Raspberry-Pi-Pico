ed409d51a6cd484fc87f7061c3a0aab86c57c52d
Alex Mous <alex.mous.dev@gmail.com>
2022-12-19 03:52:27 -0800

    Cleanup

--- example/vl53l1x_pico_w.c
+++
@@ -1,6 +0,0 @@
-#include "vl53l1x_pico.h"
-#include "pico/stdlib.h"
-
-int main() {
-    stdio_init_all();
-}
--- vl53l1x_api/vl53l1x_pico.h
+++
@@ -1,6 +0,0 @@
-#ifndef _VL53L1X_PICO_H_
-#define _VL53L1X_PICO_H_
-
-// todo: add api for Pico/Pico W
-
-#endif  // _VL53L1X_PICO_H_
